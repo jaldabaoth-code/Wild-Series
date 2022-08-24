1580971bab7c7feb9417662d9ba48d3beb2b7833
jaldabaoth-code <grialazurabi@gmail.com>
2022-08-24 02:19:57 +0200

    service fixture save

--- src/Service/FixturesData.php
+++ src/Service/FixturesData.php
@@ -51,8 +51,125 @@ class FixturesData
     const THE_WALKING_DEAD = [
         'Season 01' => [
             'number' => 1,
-            'description' => 'After being injured in the line of duty, Sheriff Rick Grimes wakes up from a weeks-long coma and finds himself in a post-apocalyptic world.',
+            'description' => 'After being injured in the line of duty, Sheriff Rick Grimes wakes up from a weeks-long coma to find a post-apocalyptic world where nearly the entire American population has turned into zombies. Alone and confused, Rick travels to Atlanta to search for his wife Lori and son Carl. On his way, he will meet other survivors with whom he will try to stay alive in this metamorphosed, frightening and perilous world.',
             'year' => 2010,
+            'actors' => [
+                'Andrew Lincoln',
+                'Sarah Wayne Callies',
+                'Jon Bernthal',
+                'Laurie Holden',
+                'Jeffrey DeMunn',
+                'Steven Yeun',
+                'Chandler Riggs'
+            ]
+        ],
+        'Season 02' => [
+            'number' => 2,
+            'description' => 'In the aftermath of the CDC explosion, Rick and his group flee Atlanta as the city is infested with zombies. Faced with a new threat, they find refuge on the farm of Hershel Greene, a man whose family hides a terrible secret. But the bonds of the group are put to the test when tensions erupt between the survivors and the inhabitants of the farm, but also between Rick and Shane.',
+            'year' => 2011,
+            'actors' => [
+                'Andrew Lincoln',
+                'Jon Bernthal',
+                'Sarah Wayne Callies',
+                'Laurie Holden',
+                'Jeffrey DeMunn',
+                'Steven Yeun',
+                'Chandler Riggs',
+                'Norman Reedus',
+                'Melissa McBride'
+            ]
+        ],
+        'Season 03' => [
+            'number' => 3,
+            'description' => 'A few months after abandoning Hershel\'s farm, Rick and his community of survivors discover an abandoned prison in which to settle. But the security of the group is quickly threatened by the neighboring community led by a ruthless man, nicknamed the Governor.',
+            'year' => 2012,
+            'actors' => [
+                'Andrew Lincoln',
+                'Sarah Wayne Callies',
+                'Steven Yeun',
+                'Laurie Holden',
+                'David Morrissey',
+                'Chandler Riggs',
+                'Norman Reedus',
+                'Danai Gurira',
+                'Melissa McBride',
+                'Scott Wilson',
+                'Lauren Cohan',
+                'Michael Rooker'
+            ]
+        ],
+        'Season 04' => [
+            'number' => 4,
+            'description' => 'Several months have passed since the attack on the Governor and life is gradually resuming its course in the prison. When a tragic event forces the survivors to separate and take different paths, survival becomes more perilous. Faced with new dangers and enemies, they will have to face difficult choices.',
+            'year' => 2013,
+            'actors' => [
+                'Andrew Lincoln',
+                'Steven Yeun',
+                'Chandler Riggs',
+                'Norman Reedus',
+                'David Morrissey',
+                'Danai Gurira',
+                'Lauren Cohan',
+                'Chad L. Coleman',
+                'Sonequa Martin-Green',
+                'Emily Kinney',
+                'Lawrence Gilliard Jr.',
+                'Melissa McBride',
+                'Scott Wilson',
+            ]
+        ],
+        'Season 05' => [
+            'number' => 5,
+            'description' => 'Captives of the Terminus community, Rick and his family manage to escape. After a long journey and many sacrifices, the group finds refuge in Alexandria, a small fortified town that has never faced the horrors of the world as it has become. But their arrival will not be seen with a very good eye, especially as Rick will try to take matters into his own hands.',
+            'year' => 2014,
+            'actors' => [
+
+            ]
+        ],
+        'Season 06' => [
+            'number' => 6,
+            'description' => 'Saison 5 De la Serie',
+            'year' => 2015,
+            'actors' => [
+
+            ]
+        ],
+        'Season 07' => [
+            'number' => 7,
+            'description' => 'Saison 5 De la Serie',
+            'year' => 2016,
+            'actors' => [
+
+            ]
+        ],
+        'Season08' => [
+            'number' => 8,
+            'description' => 'Saison 5 De la Serie',
+            'year' => 2017,
+            'actors' => [
+
+            ]
+        ],
+        'Season 09' => [
+            'number' => 9,
+            'description' => 'Saison 5 De la Serie',
+            'year' => 2018,
+            'actors' => [
+
+            ]
+        ],
+        'Season 10' => [
+            'number' => 10,
+            'description' => 'Saison 5 De la Serie',
+            'year' => 2019,
+            'actors' => [
+
+            ]
+        ],
+        'Season 11' => [
+            'number' => 11,
+            'description' => 'Saison 5 De la Serie',
+            'year' => 2021,
             'actors' => [
                 'Norman Reedus',
                 'Melissa McBride',
@@ -77,56 +194,6 @@ class FixturesData
                 'Josh Hamilton',
                 'Laurie Fortier'
             ]
-        ],
-        'Season 02' => [
-            'number' => 2,
-            'description' => 'Saison 2 De la Serie',
-            'year' => 2011
-        ],
-        'Season 03' => [
-            'number' => 3,
-            'description' => 'Saison 3 De la Serie',
-            'year' => 2012
-        ],
-        'Season 04' => [
-            'number' => 4,
-            'description' => 'Saison 4 De la Serie',
-            'year' => 2013
-        ],
-        'Season 05' => [
-            'number' => 5,
-            'description' => 'Saison 5 De la Serie',
-            'year' => 2014
-        ],
-        'Season 06' => [
-            'number' => 6,
-            'description' => 'Saison 5 De la Serie',
-            'year' => 2014
-        ],
-        'Season 07' => [
-            'number' => 7,
-            'description' => 'Saison 5 De la Serie',
-            'year' => 2014
-        ],
-        'Season08' => [
-            'number' => 8,
-            'description' => 'Saison 5 De la Serie',
-            'year' => 2014
-        ],
-        'Season 09' => [
-            'number' => 9,
-            'description' => 'Saison 5 De la Serie',
-            'year' => 2014
-        ],
-        'Season 10' => [
-            'number' => 10,
-            'description' => 'Saison 5 De la Serie',
-            'year' => 2014
-        ],
-        'Season 11' => [
-            'number' => 11,
-            'description' => 'Saison 5 De la Serie',
-            'year' => 2014
         ],
     ];
 
