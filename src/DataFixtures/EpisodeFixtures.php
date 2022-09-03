66e0e1c85df56d0039b28fdc92e8fcf15f64532d
jaldabaoth-code <grialazurabi@gmail.com>
2022-09-03 03:25:20 +0200

    delete useless comments

--- src/DataFixtures/EpisodeFixtures.php
+++ src/DataFixtures/EpisodeFixtures.php
@@ -11,34 +11,7 @@ use App\Service\Slugify;
 
 class EpisodeFixtures extends Fixture implements DependentFixtureInterface
 {
-    const EPISODES = [
-        [
-            'title' => "Le Premier",
-            'number' => 1,
-            'synopsis' => 'Synopsis du premier',
-        ],
-        [
-            'title' => "Le deuxieme",
-            'number' => 2,
-            'synopsis' => 'Synopsis du deuxieme',
-        ],
-        [
-            'title' => "Le Troisieme",
-            'number' => 3,
-            'synopsis' => 'Synopsis du troisieme',
-        ],
-        [
-            'title' => "Le Quatrieme",
-            'number' => 4,
-            'synopsis' => 'Synopsis du quatrieme',
-        ],
-        [
-            'title' => "La Cinquieme",
-            'number' => 5,
-            'synopsis' => 'Synopsis de la cinquieme',
-        ],
-    ];
-    
+
     private Slugify $slugify;
 
     public function __construct(Slugify $slugify)
--- src/DataFixtures/SeasonFixtures.php
+++ src/DataFixtures/SeasonFixtures.php
@@ -10,121 +10,7 @@ use Doctrine\Persistence\ObjectManager;
 
 class SeasonFixtures extends Fixture implements DependentFixtureInterface
 {
-    const THE_WALKING_DEAD = [
-        'Season01' => [
-            'number' => 1,
-            'description' => 'Saison 1 De la Serie',
-            'year' => 2010,
-            'actors' => [
-                'Norman Reedus',
-                'Andrew Lincoln',
-                'Lauren Cohan',
-                'Jeffrey Dean Morgan',
-                'Chandler Riggs'
-            ]
-        ],
-        'Season02' => [
-            'number' => 2,
-            'description' => 'Saison 2 De la Serie',
-            'year' => 2011
-        ],
-        'Season03' => [
-            'number' => 3,
-            'description' => 'Saison 3 De la Serie',
-            'year' => 2012
-        ],
-        'Season04' => [
-            'number' => 4,
-            'description' => 'Saison 4 De la Serie',
-            'year' => 2013
-        ],
-        'Season05' => [
-            'number' => 5,
-            'description' => 'Saison 5 De la Serie',
-            'year' => 2014
-        ],
-        'Season06' => [
-            'number' => 6,
-            'description' => 'Saison 5 De la Serie',
-            'year' => 2014
-        ],
-        'Season07' => [
-            'number' => 7,
-            'description' => 'Saison 5 De la Serie',
-            'year' => 2014
-        ],
-        'Season08' => [
-            'number' => 8,
-            'description' => 'Saison 5 De la Serie',
-            'year' => 2014
-        ],
-        'Season09' => [
-            'number' => 9,
-            'description' => 'Saison 5 De la Serie',
-            'year' => 2014
-        ],
-        'Season10' => [
-            'number' => 10,
-            'description' => 'Saison 5 De la Serie',
-            'year' => 2014
-        ],
-        'Season11' => [
-            'number' => 11,
-            'description' => 'Saison 5 De la Serie',
-            'year' => 2014
-        ],
-    ];
-
-
-    const GAME_OF_THRONES = [
-        'Season01' => ['number' => 1, 'description' => 'Saison 1 De la Serie', 'year' => 2010, 'actors' => [
-                                                                                                    'Pedro Pascal',
-                                                                                                    'Kit Harington',
-                                                                                                ]
-        ],
-        'Season02' => [ 'number' => 2, 'description' => 'Saison 2 De la Serie', 'year' => 2011
-        ],
-        'Season03' => [
-            'number' => 3,
-            'description' => 'Saison 3 De la Serie',
-            'year' => 2012
-        ],
-        'Season04' => [
-            'number' => 4,
-            'description' => 'Saison 1 De la Serie',
-            'year' => 2010,
-            'actors' => [
-                'Pedro Pascal',
-                'Kit Harington',
-            ]
-        ],
-        'Season05' => [
-            'number' => 5,
-            'description' => 'Saison 2 De la Serie',
-            'year' => 2011
-        ],
-        'Season06' => [
-            'number' => 6,
-            'description' => 'Saison 3 De la Serie',
-            'year' => 2012
-        ],
-        'Season07' => [
-            'number' => 7,
-            'description' => 'Saison 1 De la Serie',
-            'year' => 2010,
-            'actors' => [
-                'Pedro Pascal',
-                'Kit Harington',
-            ]
-        ],
-        'Season08' => [
-            'number' => 8,
-            'description' => 'Saison 3 De la Serie',
-            'year' => 2012
-        ],
-    ];
-
-    public function load(ObjectManager $manager)
+     public function load(ObjectManager $manager)
     {
         foreach (FixturesData::TV_SERIES as $seriesTitle => $seriesData) {
             foreach ($this->getSeries($seriesTitle) as $seasonNumber => $seasonData) {
@@ -151,7 +37,6 @@ class SeasonFixtures extends Fixture implements DependentFixtureInterface
         $manager->flush();
     }
 
-
     public function getSeries(string $title): array
     {
         switch ($title) {
@@ -169,53 +54,6 @@ class SeasonFixtures extends Fixture implements DependentFixtureInterface
                 return [];
         }
     }
-
-
-
-    public function getActorsForSeries(string $title, Season $program, array $seasonActors)
-    {
-        switch ($title) {
-            case 'The Walking Dead':
-                for ($i=0; $i < count($seasonActors); $i++) {
-/*                    if(in_array($number[$i], ActorFixtures::ACTORS)) {*/
-                        $program->addActor($this->getReference('actor_' . $i));
-/*                    }*/
-                }
-                break;
-            case 'Game of Thrones':
-                for ($i=0; $i < count($seasonActors); $i++) {
-                   // if (in_array(self::ACTORS_OF_GAME_OF_THRONES[$i], ActorFixtures::ACTORS)) {
-                        $program->addActor($this->getReference('actor_' . $i));
-                  //  }
-                }
-                break;/*
-            case 'Breaking Bad':
-                for ($i=0; $i < count(self::ACTORS_OF_BREAKING_BAD); $i++) {
-                    if (in_array(self::ACTORS_OF_BREAKING_BAD[$i], ActorFixtures::ACTORS)) {
-                        $program->addActor($this->getReference('actor_' . $i));
-                    }
-                }
-                break;
-            case 'Attack on Titan':
-                for ($i=0; $i < count(self::ACTORS_OF_ATTACK_ON_TITAN); $i++) {
-                    if (in_array(self::ACTORS_OF_ATTACK_ON_TITAN[$i], ActorFixtures::ACTORS)) {
-                        $program->addActor($this->getReference('actor_' . $i));
-                    }
-                }
-                break;
-            case 'The Mandalorian':
-                for ($i=0; $i < count(self::ACTORS_OF_THE_MANDALORIAN); $i++) {
-                    if (in_array(self::ACTORS_OF_THE_MANDALORIAN[$i], ActorFixtures::ACTORS)) {
-                        $program->addActor($this->getReference('actor_' . $i));
-                    }
-                }
-                break;*/
-        }
-    }
-
-
-
-
 
     public function getDependencies()
     {
