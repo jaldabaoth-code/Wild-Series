5cdba052cda0b574b83ac78c6a7cbcbd75211f1b
jaldabaoth-code <grialazurabi@gmail.com>
2022-09-08 01:08:42 +0200

    save

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -11,7 +11,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mime\Address;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Controller\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
