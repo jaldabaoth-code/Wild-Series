a01a51fd29bc39a29fb3e748c86a97cb20cb2d9c
jaldabaoth-code <grialazurabi@gmail.com>
2022-10-27 01:16:53 +0200

    save

---
+++ src/Controller/CommentController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * @Route("/comment")
+ */
+class CommentController extends AbstractController
+{
+
+}
